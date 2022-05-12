0509c6198501f46787474260421287e303496725
“Adithya <Adithyasrinivas66@gmail.comgit config --global user.email Adithyasrinivas66@gmail.com>
2022-05-12 08:31:45 +0530

    [Adithya]started working with the code

---
+++ firstshell.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -x
+
+echo "This is Adithya Srinivas"
+
+#initialize a variable
+read -p "Enter your Name"fullName
+
+echo "Enter your City"
+read city
+
+fullName="Adithya"
+
+echo $fullname
+echo $city
