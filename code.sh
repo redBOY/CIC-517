52de58f2693c359824e0789949513170e4b9ca6c
“Adithya <Adithyasrinivas66@gmail.comgit config --global user.email Adithyasrinivas66@gmail.com>
2022-05-19 16:01:32 +0530

    ifelse

---
+++ code.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -x
+
+echo "Welcome to shell script"
+
+#initialize a vriable
+read -p "Enter your name " fullName
+
+echo "Enter your city "
+read city
+
+#fullName="Mayank Singh"
+
+echo $fullName
+echo $city
