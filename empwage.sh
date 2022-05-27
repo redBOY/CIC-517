0cc347769e78c2494044658624354204688db393
“Adithya <Adithyasrinivas66@gmail.comgit config --global user.email Adithyasrinivas66@gmail.com>
2022-05-27 15:28:56 +0530

    second commit

--- empwage.sh
+++ empwage.sh
@@ -10,3 +10,19 @@ maxHrsInMonth=20
 
 totalWrkDays=0
 totalEmpHrs=0
+
+
+declare -A dailywage
+
+function getWrkHrs() {
+   local $randomCheck=$1
+   case $randomCheck in
+               $isFullTime)
+                           empHrs=8 ;;
+               $isPartTime)
+                           empHrs=4 ;;
+               *)
+                           empHrs=0 ;;
+   esac
+   echo $empHrs
+}
