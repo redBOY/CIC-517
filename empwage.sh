303466655ce7fc4d224ad1890aaadb248f2c44f4
“Adithya <Adithyasrinivas66@gmail.comgit config --global user.email Adithyasrinivas66@gmail.com>
2022-05-27 15:38:10 +0530

    fourth commit

--- empwage.sh
+++ empwage.sh
@@ -10,3 +10,32 @@ maxHrsInMonth=20
 
 totalWrkDays=0
 totalEmpHrs=0
+
+declare -A dailywage
+
+function getWrkHrs() {
+   local $randomCheck=$1
+   case $randomCheck in
+               $isFullTime)
+                           empHrs=8 ;;
+               $isPartTime)
+                           empHrs=4 ;;
+               *)
+                           empHrs=0 ;;
+   esac
+   echo $empHrs
+}
+
+function getEmpwage() {
+	local empHrs=$1
+	echo $(( $empHrs*$dailyWagePerHrs ))
+}
+
+while [[ $totalWrkDays -lt $numOfDays && $totalEmpHrs -lt  $maxHrsInMonth ]]
+do
+   ((totalWrkDays++))
+   randomCheck=$(( RANDOM%3 ));
+   empHrs="$( getWrkHrs $randomCheck )"
+   totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
+	dailywage["day"$totalWrkDays]="$( getEmpwage $empHrs )"
+done
